8c72368249966f9c32d6cb05230114821c655def
sgondge <sgondge@atecglobal.io>
2024-11-18 08:33:00 +0530

    minor adjustments ofr i2c

--- main.c
+++ main.c
@@ -34,7 +34,7 @@ int send_command_to_pbci(uint8_t plateID, uint8_t power, uint8_t temperature, ui
 int receive_data_from_pbci(uint8_t *memory_address, uint8_t *manufacturerID, uint8_t *plateIDReceived, uint8_t *errorCode, 
                            uint8_t *voltage_ms, uint8_t *voltage_ls, uint8_t *current, uint8_t *wattHour_ms,
                            uint8_t *wattHour_ls, uint8_t *igbtTemp, uint8_t *glassSurfaceTemp_ms, uint8_t *glassSurfaceTemp_ls,
-													 uint8_t *crc16_ms, uint8_t *crc16_ls, uint8_t padding_1, uint8_t padding_2, uint8_t padding_3); 
+													 uint8_t *crc16_ms, uint8_t *crc16_ls, uint8_t *padding_1, uint8_t *padding_2, uint8_t *padding_3); 
 //unsigned int GetCRC16(volatile unsigned char *puchMsg, unsigned int DataLen);
 void updateParametersAndSendCommand(void);
 void Clear_I2C_Buffer(void);
@@ -85,7 +85,7 @@ u8 Tx_Index = 0;
 u8 Rx_Index = 0;
 
 //volatile unsigned int CRC_16_value = 0;
-uint8_t plateID, power, temperature;
+uint8_t plateID, powerSend, temperature;
 uint8_t crc16_ms, crc16_ls, padding_1, padding_2, padding_3;
 uint8_t *memory_address, *manufacturerID, *plateIDReceived, *errorCode, *igbtTemp, *voltage_ms, *voltage_ls, *current, *wattHour_ms, *wattHour_ls, *glassSurfaceTemp_ms, *glassSurfaceTemp_ls, *crc16ms, *crc16ls;
 
@@ -98,11 +98,11 @@ int main(void)
 {
   I2C_Configuration();                /* I2C configuration                                                  */
 	plate_req_Configuration();
-	plateID = 0;
-	power = 0;
-	temperature = 0;
-	crc16_ms= 0;
-	crc16_ls= 0;
+//	plateID = 0;
+//	power = 0;
+//	temperature = 0;
+//	crc16_ms= 0;
+//	crc16_ls= 0;
 	padding_1= 0;
 	padding_2= 0;
 	padding_3 = 0;
@@ -112,10 +112,6 @@ int main(void)
   while (1){		
 			I2C_Slave_Receiver();
 			updateParametersAndSendCommand();
-//			receive_data_from_pbci(memory_address,  manufacturerID,  plateIDReceived,  errorCode, 
-//                           voltage_ms,  voltage_ls,  current,  wattHour_ms, wattHour_ls,
-//                           igbtTemp,  glassSurfaceTemp_ms, glassSurfaceTemp_ls,
-//  												  crc16ms, crc16ls, padding_2, padding_2, padding_2);
 	}
 }
 
@@ -137,16 +133,16 @@ void Clear_I2C_Buffer(void) {
 
 
 void updateParametersAndSendCommand() {	
-    u8 i = 0;
+
 		unsigned int CRC_16_value = 0;
 		unsigned char Data[3];
-		uint8_t power = 9;
-    uint8_t plateID = 3;
-    uint8_t temperature = 0x00; 	// Example temperature value
-		uint8_t padding_1 	= 0x00; 	// 0 padding value
+		powerSend = 9;
+    plateID = 3;
+    temperature = 0x00; 	// Example temperature value
+		padding_1 	= 0x00; 	// 0 padding value
 		
 		Data[0] = plateID;
-		Data[1]	= power;
+		Data[1]	= powerSend;
 		Data[2] = temperature;
 
 		CRC_16_value = CRC_GetModbus16(Data, sizeof(Data));
@@ -162,9 +158,7 @@ void updateParametersAndSendCommand() {
 //		I2C_Slave_Buffer_Tx[6] = padding_1;
 //		I2C_Slave_Buffer_Tx[7] = padding_1;
 	
-			for (i = 0; i <= BufferSize; i++){
-					send_command_to_pbci(plateID, power, temperature, crc16_ms, crc16_ls, padding_1, padding_1, padding_1);
-				}
+		send_command_to_pbci(plateID, powerSend, temperature, crc16_ms, crc16_ls, padding_1, padding_1, padding_1);
 	}
 
 unsigned short GetCrc_16(unsigned char * pData, int nLength, unsigned short init, const unsigned short *ptable)
@@ -209,8 +203,8 @@ int send_command_to_pbci(uint8_t plateID, uint8_t power, uint8_t temperature,uin
 		I2C_Slave_Buffer_Tx[0] = plateID;              // Plate ID (1 or 2)
     I2C_Slave_Buffer_Tx[1] = power;                // Power level (0 to 10)
     I2C_Slave_Buffer_Tx[2] = temperature;          // Temperature value (0 to 240)
-    I2C_Slave_Buffer_Tx[3] = crc16_ms;                    // CRC High Byte (set to 0 for now)
-    I2C_Slave_Buffer_Tx[4] = crc16_ls;                    // CRC Low Byte (set to 0 for now)
+    I2C_Slave_Buffer_Tx[3] = crc16_ls;                    // CRC High Byte (set to 0 for now)
+    I2C_Slave_Buffer_Tx[4] = crc16_ms;                    // CRC Low Byte (set to 0 for now)
     I2C_Slave_Buffer_Tx[5] = padding_1;                    // Padding
     I2C_Slave_Buffer_Tx[6] = padding_2;                    // Padding
     I2C_Slave_Buffer_Tx[7] = padding_3;                    // Padding
@@ -223,7 +217,7 @@ int send_command_to_pbci(uint8_t plateID, uint8_t power, uint8_t temperature,uin
 int receive_data_from_pbci(uint8_t *memory_address, uint8_t *manufacturerID, uint8_t *plateIDReceived, uint8_t *errorCode, 
                            uint8_t *voltage_ms, uint8_t *voltage_ls, uint8_t *current, uint8_t *wattHour_ms,
                            uint8_t *wattHour_ls, uint8_t *igbtTemp, uint8_t *glassSurfaceTemp_ms, uint8_t *glassSurfaceTemp_ls,
-													 uint8_t *crc16_ms, uint8_t *crc16_ls, uint8_t padding_1, uint8_t padding_2, uint8_t padding_3){
+													 uint8_t *crc16_ms, uint8_t *crc16_ls, uint8_t *padding_1,uint8_t *padding_2,uint8_t *padding_3){
 		
 		I2C_Slave_Receiver();	 
     // Parse received data into respective fields (as described earlier)
@@ -241,9 +235,9 @@ int receive_data_from_pbci(uint8_t *memory_address, uint8_t *manufacturerID, uin
 		*glassSurfaceTemp_ls	= I2C_Slave_Buffer_Rx[11];
 		*crc16_ms 						= I2C_Slave_Buffer_Rx[12];										 
     *crc16_ls 						= I2C_Slave_Buffer_Rx[13];  // Set CRC to 0 for now, since CRC is not calculated currently
-		padding_1 						= I2C_Slave_Buffer_Rx[14];
-		padding_2 						= I2C_Slave_Buffer_Rx[15];
-		padding_3 						= I2C_Slave_Buffer_Rx[16];	
+		*padding_1 						= I2C_Slave_Buffer_Rx[14];
+		*padding_2 						= I2C_Slave_Buffer_Rx[15];
+		*padding_3 						= I2C_Slave_Buffer_Rx[16];	
 														 
 	return 0;
 }
@@ -334,7 +328,6 @@ int clear_request_line(uint8_t plate) {
 void I2C_Slave_Receiver(void)
 {
   /* Check on Slave Receiver ADRS condition and clear it, added timeout condition                           */
-	u8 t;
 	u32 reg;
 	uint32_t timeout; 
 	timeout = TIMEOUT_VALUE;
@@ -388,17 +381,17 @@ void I2C_Slave_Transmitter(void)
 
 		while (!I2C_CheckStatus(HTCFG_I2C_SLAVE_PORT, I2C_SLAVE_ACK_TRANSMITTER_ADDRESS)){
 			reg = HTCFG_I2C_SLAVE_PORT ->SR;
-			if(reg & I2C_FLAG_TXDE){	
-					I2C_SendData(HTCFG_I2C_SLAVE_PORT, I2C_Slave_Buffer_Tx[Tx_Index++]);} //TXDE condition handle
+			if(reg & I2C_FLAG_TXDE){I2C_SendData(HTCFG_I2C_SLAVE_PORT, I2C_Slave_Buffer_Tx[Tx_Index++]);} //TXDE condition handle
 			else if((reg & I2C_FLAG_TXNRX) == 0){return;}
 			else if(--timeout == 0) return;
 		}
-		while (Tx_Index <= BufferSize)
-		{
+
 		/* Check on Master Transmitter TXDE condition                                                           */
 			while (!I2C_CheckStatus(HTCFG_I2C_SLAVE_PORT, I2C_SLAVE_TX_EMPTY)){
 			if(--timeout == 0) return;
 			}
+		while (Tx_Index <= BufferSize)
+		{
 			/* Send I2C data                                                                                       */
 			I2C_SendData(HTCFG_I2C_SLAVE_PORT, I2C_Slave_Buffer_Tx[Tx_Index++]);
 			if (Tx_Index >= BufferSize) {
@@ -406,13 +399,13 @@ void I2C_Slave_Transmitter(void)
       }
 		}
 	  /* Check on Slave Receiver STO condition                                                                  */
-  while (!I2C_CheckStatus(HTCFG_I2C_SLAVE_PORT, I2C_SLAVE_STOP_DETECTED)){
+		while (!I2C_CheckStatus(HTCFG_I2C_SLAVE_PORT, I2C_SLAVE_STOP_DETECTED)){
 		 // Reset I2C peripheral if bus is busy
-		if (I2C_GetFlagStatus(HTCFG_I2C_SLAVE_PORT, I2C_FLAG_BUSBUSY)) {
-			I2C_Reset();
-			return;
+			if (I2C_GetFlagStatus(HTCFG_I2C_SLAVE_PORT, I2C_FLAG_BUSBUSY)) {
+				I2C_Reset();
+				return;
+			}
+			if(--timeout == 0) return;
 		}
-		if(--timeout == 0) return;
-	}
 }
 
